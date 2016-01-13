a516b87e2e0674d91300981862dabbd15e3eab73
Anna Tomka <tomkaanna@gmail.com>
2016-01-13 17:07:47 +0100

    Add example source files

---
+++ interval-example/script.js
@@ -0,0 +1,15 @@
+var intervalStream = Rx.Observable.interval(1000)
+  .filter(function(value){
+    return value%2===0;
+  })
+  .take(5)
+  .map(function(value){
+    return value+".";
+  });
+
+var cntLabel = document.querySelector("h1");
+
+intervalStream.subscribe(function(result){
+  cntLabel.textContent = result;
+});
+
---
+++ spotify-autocomplete-example/script.js
@@ -0,0 +1,36 @@
+var input = document.querySelector("input");
+var resultList = document.getElementById("list");
+
+var inputStream = Rx.Observable.fromEvent(input,"keyup");
+
+var filterInputStream = inputStream.debounce(250)
+.map(function(){
+  return input.value;
+})
+.filter(function(value){
+  return value;
+})
+.distinctUntilChanged();
+
+function querySpotify(value){
+  return Rx.Observable.fromPromise(
+    $.getJSON("https://api.spotify.com/v1/search?q="+value + "&type=artist"));
+}
+
+var responseStream = filterInputStream
+.flatMapLatest(querySpotify).doOnNext(clearList);
+
+
+var artistStream = responseStream.flatMap(function(result){
+  return Rx.Observable.fromArray(result.artists.items).pluck("name");
+});
+
+artistStream.subscribe(renderResult);
+
+function renderResult(name){
+  resultList.innerHTML += '<li>'+name + '</li>';
+}
+
+function clearList(){
+  resultList.innerHTML = '';
+}
